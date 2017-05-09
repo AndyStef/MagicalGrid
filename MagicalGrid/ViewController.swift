e476788855867de1301c40c54d31c6bec7e78f19
AndyStef <Andy.stef.2512@gmail.com>
2017-05-09 13:01:18 +0300

    initial commit

---
+++ MagicalGrid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicalGrid
+//
+//  Created by Stef on 5/9/17.
+//  Copyright © 2017 Stef. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
